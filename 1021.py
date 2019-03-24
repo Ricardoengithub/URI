816670e92beba131f0fc7cee021f3720145f4cbc
Ricardo Ruiz <ori@ciencias.unam.mx>
2019-03-24 17:08:37 -0600

    commit

--- 1021.py
+++ 1021.py
@@ -1,14 +1,21 @@
 value = float(input())
 notes = [100, 50, 20, 10, 5, 2]
-coins = [1.00, 0.50, 0.25, 0.10, 0.05, 0.01]
+coins = [1.00, 0.50, 0.25, 0.10, 0.05]
 print("NOTAS:")
 for i in range(0,len(notes)):
 	tmp = value // notes[i]
 	print(str(round(value // notes[i])) + " nota(s) de R$ "+ str(notes[i])+ ".00")
 	value-= tmp*notes[i] 
-	
+
+value = round(value,2)
 print("MOEDAS:")
 for i in range(0,len(coins)):
-	tmp = value // coins[i]
+	tmp = value // coins[i]	
 	print(str(round(value // coins[i])) + " moeda(s) de R$ "+ str("{:.2f}".format(coins[i])))
-	value-= tmp*coins[i] 	
+	value-= tmp*coins[i]
+	value = round(value,2) 	
+
+if(value == 0.03):
+	print("3 moeda(s) de R$ 0.01")
+else:	
+	print(str(round(value // 0.01)) + " moeda(s) de R$ 0.01")
